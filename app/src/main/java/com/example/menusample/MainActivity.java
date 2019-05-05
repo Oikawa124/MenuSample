2ece966cec861ab66f73809c483b41aa23c24fdb
devel <oiku1515@gmail.com>
2019-05-05 11:59:04 +0900

    IntentSampleの実装の部分まで完了

--- app/src/main/java/com/example/menusample/MainActivity.java
+++ app/src/main/java/com/example/menusample/MainActivity.java
@@ -3,11 +3,102 @@ package com.example.menusample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.app.LauncherActivity;
+import android.content.Intent;
+import android.support.v4.util.ArrayMap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+
+    // リストビューを表すフィールド
+    private ListView _lvMenu;
+
+    // リストビューに表示するリストデータ
+    private List<Map<String, Object>> _menuList;
+
+    // SimpleAdapterの第4引数に使用する定数フィールド
+    private static final String[] FROM = {"name", "price"};
+
+    // SimpleAdapterの第5引数に使用する定数フィールド
+    private static final int[] TO = {R.id.tvmenuName, R.id.tvMenuPrice};
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 画面部品ListViewを取得し、フィールドに格納
+        _lvMenu = findViewById(R.id.lvMenu);
+
+        // 定数フィールドListオブジェクトをpriveteメソッドを利用して用意し、フィールドの格納
+
+        _menuList = createTeishokuList();
+
+        // SimpleAdapterの生成
+
+        SimpleAdapter adapter = new SimpleAdapter(MainActivity.this, _menuList,
+                R.layout.row, FROM, TO);
+
+        // アダプタの登録
+        _lvMenu.setAdapter(adapter);
+        _lvMenu.setOnItemClickListener(new ListItemClickListener());
+
+    }
+
+    private List<Map<String, Object>> createTeishokuList() {
+        // SimpleAdapterで使用するListオブジェクトを用意。
+        List<Map<String, Object>> menuList = new ArrayList<>();
+
+        // 定食のデータを格納する
+        Map<String, Object> menu = new HashMap<>();
+        menu.put("name", "唐揚げ定食");
+        menu.put("price", 800);
+        menu.put("desc", "若鳥の唐揚げにサラダ、ご飯とみそ汁がつきます。");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "ハンバーグ定食");
+        menu.put("price", 940);
+        menu.put("desc", "手ごねハンバーグにサラダ、ご飯とみそ汁がつきます。");
+        menuList.add(menu);
+
+        return menuList;
+    }
+
+    private class ListItemClickListener implements android.widget.AdapterView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            // タップされた行のデータを取得
+            Map<String, Object> item = (Map<String, Object>) parent.getItemAtPosition(position);
+
+            // 定食と金額を取得
+
+            String menuName = (String) item.get("name");
+            Integer menuPrice = (Integer) item.get("price");
+
+
+            // インテントオブジェクトを生成
+            Intent intent = new Intent(MainActivity.this, MenuThanksActivity.class);
+
+            // 第2画面に送るデータを格納
+            intent.putExtra("menuName", menuName);
+            intent.putExtra("menuPrice", menuPrice + "円");
+
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/com/example/menusample/MenuThanksActivity.java
@@ -0,0 +1,41 @@
+package com.example.menusample;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MenuThanksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_thanks);
+
+
+        // インテントオブジェクトの取得
+        Intent intent = getIntent();
+
+        // リスト画面から渡されたデータを取得
+        String menuName = intent.getStringExtra("menuName");
+        String menuPrice = intent.getStringExtra("menuPrice");
+
+        // 定食名と金額を表示させるtextviewを取得
+
+        TextView tvMenuName = findViewById(R.id.tvmenuName);
+        TextView tvMenuPrice = findViewById(R.id.tvMenuPrice);
+
+        // textViewに定食名と金額を表示
+
+        tvMenuName.setText(menuName);
+        tvMenuPrice.setText(menuPrice);
+
+    }
+
+    // 戻るボタンをタップ
+
+    public void onbackButton(View view) {
+        finish();
+    }
+}
